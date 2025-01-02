2c17ad16f3e94a7e14645a6b435e77e971932229
Nazar <nazarbokalo0@gmail.com>
2025-01-02 13:24:54 +0200

    firs commit

---
+++ src/main/java/com/football_manager/controller/TeamController.java
@@ -0,0 +1,15 @@
+package com.football_manager.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/teams")
+public class TeamController {
+
+    @GetMapping
+    public String test() {
+        return "test";
+    }
+}
